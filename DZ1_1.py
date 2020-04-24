eb322723a56bf8fc2083b5f6d2eeeb30653a76ea
Mangolime <nina008@mail.ru>
2020-04-24 17:45:52 +0300

    Работа с переменными

---
+++ DZ1_1.py
@@ -0,0 +1,10 @@
+a = 'April'
+b = '24th'
+c = 2020
+print(a, b, c)
+age_year = int(input('Your age (years): '))
+age_month = int(input('Your age (months): '))
+name = input('Your name: ')
+surname = input('Your surname: ')
+print('User: ', name, surname)
+print('Age: ', age_year, 'years,', age_month, 'months')
